811f1a327d6df57162522d755210829384957885
ArnaudCs <arnaud.cossu@gmail.com>
2023-03-14 19:24:00 +0100

    Maj Site

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio/'
+    ? '/Arnaudcs.github.io/'
     : '/',
   transpileDependencies: true,
 
